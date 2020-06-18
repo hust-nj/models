678fea4ce3e6bb98d3a70e4d82e6de2ce2badeb4
hust-nj <2402224349@qq.com>
2020-06-18 15:48:50 +0800

    add coco17 mask dataset support

---
+++ research/deeplab/datasets/convert_coco17.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Script to download and preprocess the PASCAL VOC 2012 dataset.
+#
+# Usage:
+#   bash ./convert_coco17.sh
+#
+# The folder structure is assumed to be:
+#  + datasets
+#     - build_data.py
+#     - build_coco17_data.py
+#     - convert_coco17.sh
+#     - remove_gt_colormap.py
+#     + coco_seg
+#       + coco17
+#         + images
+#         + SegmentationClass
+#
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+CURRENT_DIR=$(pwd)
+WORK_DIR="./coco_seg"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+mkdir -p "${WORK_DIR}"
+COCO_ROOT="${WORK_DIR}/coco17"
+
+# Build TFRecords of the dataset.
+# First, create output directory for storing TFRecords.
+OUTPUT_DIR="${WORK_DIR}/tfrecord"
+mkdir -p "${OUTPUT_DIR}"
+
+LIST_FOLDER="${COCO_ROOT}"
+
+echo "Converting coco17 dataset..."
+python3 "${SCRIPT_DIR}/build_coco17_data.py" \
+  --list_folder="${LIST_FOLDER}" \
+  --image_format="jpg" \
+  --label_format="png" \
+  --output_dir="${OUTPUT_DIR}"
